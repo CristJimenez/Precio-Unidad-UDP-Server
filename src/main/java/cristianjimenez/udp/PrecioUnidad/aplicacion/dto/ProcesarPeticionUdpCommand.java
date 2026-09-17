ea84d5c51337c0e764d64edc74dbe1f7e7b9a787
CristJimenez <123cristianjose@gmail.com>
2026-09-17 07:28:45 -0500

    aplicacion - data transfer objects

---
+++ src/main/java/cristianjimenez/udp/PrecioUnidad/aplicacion/dto/ProcesarPeticionUdpCommand.java
@@ -0,0 +1,4 @@
+package cristianjimenez.udp.PrecioUnidad.aplicacion.dto;
+
+public record ProcesarPeticionUdpCommand(String ipCliente, int puertoCliente,String mensaje) {
+}
--- src/main/java/cristianjimenez/udp/PrecioUnidad/dominio/puertos/salida/ControladorServidorRedPort.java
+++ src/main/java/cristianjimenez/udp/PrecioUnidad/dominio/puertos/salida/ControladorServidorRedPort.java
@@ -1,9 +1,10 @@
 package cristianjimenez.udp.PrecioUnidad.dominio.puertos.salida;
 
+import cristianjimenez.udp.PrecioUnidad.aplicacion.excepciones.ServidorRedException;
 import cristianjimenez.udp.PrecioUnidad.dominio.vo.PuertoRed;
 
 public interface ControladorServidorRedPort {
-    //void iniciar(PuertoRed puerto) throws ServidorRedException;
+    void iniciar(PuertoRed puerto) throws ServidorRedException;
 
     void detener();
 
